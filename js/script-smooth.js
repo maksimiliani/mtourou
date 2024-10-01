681d36814bc2eaa75bb41d7a19b7133c2a5f0c35
maksimiliani <40344223+maksimiliani@users.noreply.github.com>
2024-09-30 19:44:30 -0600

    initial release

---
+++ js/script-smooth.js
@@ -0,0 +1,179 @@
+const frames_p = 60;
+
+let nIntervId;
+let startValue, endValue, aniimation_time;
+
+let cell_tmp = {col: "", row: ""};
+let oldmap = {col: "", row: ""};
+let beforeopenmap = {col: "", row: ""};
+
+let cursorX, cursorY;
+let cell = {lastTriggered: null};
+let animation_interrupted = false;
+let element_pos_p;
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    let htm = document.querySelector('html[data-scheme]');
+    htm.setAttribute('data-scheme', "dark");
+  }
+
+  if (cell_tmp.col == "") {
+    for (let i = 1; i <= getStringCount(window.getComputedStyle(document.querySelector('.container'), null)["grid-template-columns"], "px"); i++) {
+      cell_tmp.col += '1fr ';
+    }
+    for (let i = 1; i <= getStringCount(window.getComputedStyle(document.querySelector('.container'), null)["grid-template-rows"], "px"); i++) {
+      cell_tmp.row += '1fr ';
+    }
+    oldmap = cell_tmp;
+    document.querySelector('.container').style.gridTemplateColumns = oldmap.col;
+    document.querySelector('.container').style.gridTemplateRows = oldmap.row;
+  }
+  
+  document.getElementById('container').addEventListener("mousemove", handleMouseMove);
+  document.getElementById('container').addEventListener("mouseout", handleMouseOut);
+  document.querySelectorAll('.container > div').forEach(item => {
+    item.addEventListener("mousemove", handleMouseOver);
+    item.addEventListener("click", (e) => {
+      expandCell(e, false);
+    });
+    });
+  
+  startValue = {x: window.innerWidth * 1.5, y: window.innerHeight * 1.5};
+  endValue = {x: 0, y: 0};
+  aniimation_time = {duration: 500, currentTime: 0};
+  
+  nIntervId = setInterval(() => {
+    animate()
+    }, 50);
+
+});
+
+function expandCell(target, smooth = false, double_click = true, scale = "15") {
+  if ((cell.lastTriggered == target.target) && !double_click && !smooth) return;
+  if (beforeopenmap.col == "") {
+    for (let i = 1; i <= getStringCount(window.getComputedStyle(document.querySelector('.container'), null)["grid-template-columns"], "px"); i++) {
+      beforeopenmap.col += '1fr ';
+    }
+    for (let i = 1; i <= getStringCount(window.getComputedStyle(document.querySelector('.container'), null)["grid-template-rows"], "px"); i++) {
+      beforeopenmap.row += '1fr ';
+    }
+    oldmap = beforeopenmap;
+  }
+
+  let newmap = {col: "", row: ""};
+
+  let inx = {
+    h:1 + getOrderIndex(target.target)%getStringCount(oldmap.col, "fr"),
+    v:1 + Math.floor(getOrderIndex(target.target)/getStringCount(oldmap.col, "fr"))
+  };
+
+  let maxcount = {
+    col: getStringCount(oldmap.col, "fr"),
+    row: getStringCount(oldmap.row, "fr"),
+  }
+  for (let i = 1; i <= maxcount.col; i++) {
+    if (smooth && (i + 1 == inx.h )) {
+      newmap.col += (scale*element_pos_p.h + (maxcount.col - Math.abs(inx.h - i))/(maxcount.col)*(1 - element_pos_p.h) + 'fr ');
+    } else if (smooth && (i - 1 == inx.h )) {
+      newmap.col += (scale*(1 - element_pos_p.h) + (maxcount.col - Math.abs(inx.h - i))/(maxcount.col)*element_pos_p.h + 'fr ');
+    } else {
+      newmap.col += ((i == inx.h) ? (scale + 'fr ') : (((maxcount.col - Math.abs(inx.h - i))/(maxcount.col)) + 'fr '));
+    }
+  }
+  for (let i = 1; i <= maxcount.row; i++) {
+    if (smooth && (i + 1 == inx.v )) {
+      newmap.row += (scale*element_pos_p.v + (maxcount.row - Math.abs(inx.v - i))/(maxcount.row)*(1 - element_pos_p.v) + 'fr ');
+    } else if (smooth && (i - 1 == inx.v )) {
+      newmap.row += (scale*(1 - element_pos_p.v) + (maxcount.row - Math.abs(inx.v - i))/(maxcount.row)*element_pos_p.v + 'fr ');
+    } else {
+      newmap.row += ((i == inx.v) ? (scale + 'fr ') : (((maxcount.row - Math.abs(inx.v - i))/(maxcount.row)) + 'fr '));
+    }
+  }
+  
+    if ((newmap.col == oldmap.col) && (newmap.row == oldmap.row) && double_click) {
+      document.querySelector('.container').style.gridTemplateColumns = beforeopenmap.col;
+      document.querySelector('.container').style.gridTemplateRows = beforeopenmap.row;
+      beforeopenmap.col = "";
+      beforeopenmap.row = "";
+    } else {
+      document.querySelector('.container').style.gridTemplateColumns = newmap.col;
+      document.querySelector('.container').style.gridTemplateRows = newmap.row;
+      beforeopenmap = oldmap;
+    }
+
+    oldmap = newmap;
+
+    cell.lastTriggered = target.target;
+}
+
+function animate() {
+  if (animation_interrupted) {
+    clearInterval(nIntervId);
+    return;
+  }
+  // Update currentTime (you may use requestAnimationFrame for smoother animations)
+  aniimation_time.currentTime += aniimation_time.duration/frames_p; // Assuming 60 frames per second
+
+  // Calculate deducted value with ease effect
+  let deductedValue = deductWithValueEase(startValue, endValue, aniimation_time.duration, aniimation_time.currentTime);
+  document.querySelectorAll('.container > div').forEach(item => {
+    let itemRect = item.getBoundingClientRect();
+    let dx = deductedValue.x - itemRect.x;
+    let dy = deductedValue.y - itemRect.y;
+    let opacity = calculateOpacity(Math.sqrt(dx * dx + dy * dy));
+    item.style.backgroundColor = `rgba(255, 122, 0, ${opacity})`;
+  });
+  // Check if the animation is still in progress
+  if (aniimation_time.currentTime >= aniimation_time.duration) {
+    clearInterval(nIntervId);
+  }
+}
+
+function deductWithValueEase(startValue, endValue, duration, currentTime) {
+  aniimation_time.currentTime = Math.min(duration, currentTime);
+  let easedProgress = easeInOutQuad(aniimation_time.currentTime / duration);
+  return {x: startValue.x + (endValue.x - startValue.x) * easedProgress, y: startValue.y + (endValue.y - startValue.y) * easedProgress};
+}
+
+
+
+    function handleMouseMove(event) {
+      cursorX = event.clientX;
+      cursorY = event.clientY;
+      //updateRoundedCorners();
+    }
+
+    function handleMouseOut(event) {
+      document.querySelector('.container').style.gridTemplateColumns = cell_tmp.col;
+      document.querySelector('.container').style.gridTemplateRows = cell_tmp.row;
+      cell.lastTriggered = null;
+    }
+
+    function updateRoundedCorners() {
+      let gridItems = document.querySelectorAll('.container > div');
+      let containerRect = document.getElementById('container').getBoundingClientRect();
+
+      gridItems.forEach(item => {
+        let roundedCornersX = (cursorX - containerRect.left) / containerRect.width * 100 + '%';
+        let roundedCornersY = (cursorY - containerRect.top) / containerRect.height * 100 + '%';
+        item.style.borderRadius = `${roundedCornersY} 0 ${roundedCornersX} ${roundedCornersY}`;
+      });
+    }
+
+    function handleMouseOver(e) {
+      animation_interrupted = true;
+      element_pos_p = {
+        h: Math.abs((cursorX - e.target.getBoundingClientRect().x)/e.target.getBoundingClientRect().width),
+        v: Math.abs((cursorY - e.target.getBoundingClientRect().y)/e.target.getBoundingClientRect().height),
+      }
+      console.log(element_pos_p);
+
+      document.querySelectorAll('.container > div').forEach(item => {
+        let distance = calculateDistance(e.target, item);
+        let opacity = calculateOpacity(distance);
+        item.style.backgroundColor = `rgba(255, 122, 0, ${opacity})`;
+      });
+
+      expandCell(e, true, false, "1.2");
+    }
---
+++ js/utils.js
@@ -0,0 +1,36 @@
+function easeOutElastic(t) {
+    const c4 = (2 * Math.PI) / 3;
+    return t === 0
+      ? 0
+      : t === 1
+      ? 1
+      : Math.pow(2, -10 * t) * Math.sin((t * 10 - 0.75) * c4) + 1;
+}
+
+function easeInOutQuad(x) {
+return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+}
+
+function getStringCount(string, lookfor) {;
+    return string.match(new RegExp(lookfor, 'g')).length
+}
+
+function getOrderIndex(node) {
+    return Array.prototype.indexOf.call(node.parentNode.children, node);
+  }
+
+  function calculateDistance(target, item) {
+    let targetRect = target.getBoundingClientRect();
+    let itemRect = item.getBoundingClientRect();
+    let dx = targetRect.x - itemRect.x;
+    let dy = targetRect.y - itemRect.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  function calculateOpacity(distance) {
+    // Adjust the parameters of the parabolic function as needed
+    let maxDistance = window.innerWidth * 0.9; // Maximum distance at which opacity is 0
+    let minOpacity = 0; // Minimum opacity
+    let opacity = 1 - Math.pow(distance / maxDistance, 1);
+    return Math.max(minOpacity, opacity);
+  }
